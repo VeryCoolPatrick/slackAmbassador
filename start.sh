eaa165871fafa5ed51b5a0342524031f15ef5311
VeryCoolPatrick <culmerpatrick@gmail.com>
2023-08-17 16:52:20 +0100

    Added .env.example and start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+pip3 install slack_bolt
+pip3 install slack_sdk
+pip3 install pymsteams
+pip3 install slackdown
+pip3 install git+https://github.com/VeryCoolPatrick/emoji-data-python
+
+python3 server.py
